2744cd81e6b73daedab3f4cced32658ae000e54b
m-meena <meena58apt@gmail.com>
2021-10-01 17:53:17 +0530

    delete and update terms create in table

--- src/components/studentsPage.vue
+++ src/components/studentsPage.vue
@@ -8,7 +8,8 @@
              <b-form-input v-model="student.age" placeholder="Enter age"></b-form-input>
              <b-form-input v-model="student.gender" placeholder="Enter gender"></b-form-input>
              <b-form-input v-model="student.email" placeholder="Enter email"></b-form-input>
-             <button class="btn btn-primary mt-3" @click=putStudent()>submit</button>
+             <button class="btn btn-primary mt-3" @click="putStudent()">submit</button>
+             <button class="btn btn-primary mt-3 float-end" @click="putStudent()">update</button>
         </div>
      </div>
      <div class="mt-2">
@@ -19,6 +20,7 @@
                     <th>Age</th>
                     <th>Gender</th>
                     <th>Email</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -27,6 +29,10 @@
                     <td>{{s.age}}</td>
                     <td>{{s.gender}}</td>
                     <td>{{s.email}}</td>
+                    <td>
+                        <span><button class="btn btn-secondary" @click="getStudent(s.id)">edit</button></span>
+                        <span><button class="btn btn-secondary" @click="deleteStudent(s.id)">delete</button></span>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -69,8 +75,8 @@ export default {
                     authAxios
                         .post("/student/insert",this.student, config)
                         .then(response => { 
-                             alert(response.data);  
-                             this.getAllStudents();
+                             alert(response.data);
+                             this.getAllStudents();  
                              this.student.name = "";
                              this.student.age = "";
                              this.student.gender = "";
@@ -92,13 +98,47 @@ export default {
                     url: '/student/getAll',
                 }).then((response) => {
                     this.students = response.data;
-                    console.log(this.students());
+                    
                     resolve(response);
                 }).catch((err) => {
                     reject(err);
                 });
             });
-        }           
+        },
+        deleteStudent: function(id){
+            var ax1 = axios.create({
+                baseURL: "http://localhost:9090",
+            });
+            return new Promise((resolve, reject) => {
+                ax1({
+                    method: 'get',
+                    url: '/student/delete/'+id,
+                }).then((response) => {
+                    alert("Student deleted" + id); 
+                    this.getAllStudents();                                                      
+                    resolve(response);
+                }).catch((err) => {
+                    reject(err);
+                }); 
+            });      
+        },
+        getStudent: function(id){
+            var ax = axios.create({
+                baseURL: "http://localhost:9090",
+            });
+            return new Promise((resolve, reject) => {
+                ax({
+                    method: 'get',
+                    url: '/student/get/' + id,
+                }).then((response) => {
+                    console.log("Student is ", response.data)    
+                    this.student = response.data;                                     
+                    resolve(response);
+                }).catch((err) => {
+                    reject(err);
+                });                
+            });              
+        }                
     }
 }
       
